9e3fcdb09cf1fdf322b936c4a0b9065f2265a06f
Dmitrii Shvetsov <Shvec335@gmail.com>
2021-06-20 11:58:21 +0300

    Генерация разметки похожих объявлений

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 import { SIMILAR_AD_COUNT, createAd } from './data.js';
+import { createCard } from './card.js';
 
 const similarAds = new Array(SIMILAR_AD_COUNT).fill(null).map(createAd);
 
-similarAds;
+const similarListAd = document.querySelector('#map-canvas');
+
+similarListAd.appendChild(createCard(similarAds[0]));
